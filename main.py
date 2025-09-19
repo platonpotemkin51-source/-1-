0d34dcc1f573cf70f00d4ec493bac6794ab2fcca
platonpotemkin51-source <platonpotemkin51@gmail.com>
2025-09-19 14:03:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def two_sum(nums,target):
+def two_sum(nums, target):
   if type(nums) != type([]):
     raise ValueError('Ошибка! Неверный тип данных.')
   if type(target) != type(1):
@@ -6,11 +6,11 @@ def two_sum(nums,target):
   for i in range(len(nums)):
     if type(nums[i]) != type(1):
       raise ValueError('Ошибка! Неверный тип данных.')
-  if len(nums)<2:
+  if len(nums) < 2:
     raise ValueError('Ошибка! Недостаточно чисел в nums.')
 
-  for i in range(len(nums)-1):
-    for j in range(i+1,len(nums)):
-      if nums[i]+nums[j] == target:
-        res = [i,j]
+  for i in range(len(nums) - 1):
+    for j in range(i + 1, len(nums)):
+      if nums[i] + nums[j] == target:
+        res = [i, j]
         return res
