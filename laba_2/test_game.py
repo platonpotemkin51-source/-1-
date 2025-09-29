a99a847846fdd19aca1527330c4ebb7f9423b991
platonpotemkin51-source <platonpotemkin51@gmail.com>
2025-09-29 15:16:23 +0300

    Update test_game.py

--- laba_2/test_game.py
+++ laba_2/test_game.py
@@ -5,12 +5,14 @@ from python.laba_2.game import *
 class TestMySolutionG(unittest.TestCase):
 
     def test_perbor(self):
+        """провека функции долгого перебора"""
         self.assertEqual(perebor(5, [2, 3, 4, 5, 6, 7]), [5, 4])
         self.assertEqual(perebor(5, [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]), [5, 8])
         self.assertEqual(perebor(5, [-2, -1, 0, 1, 2, 3, 4]), None)
         self.assertEqual(perebor(5, [6, 7, 8, 9]), None)
 
     def test_bitperebor(self):
+        """провека функции бинарного переборп"""
         self.assertEqual(bitperebor(5, [2, 3, 4, 5, 6, 7]), [5, 2])
         self.assertEqual(bitperebor(5, [-5, -4, -3, -1, 0, 1, 2, 3, 4, 5, 6]),
                          [5, 4])
@@ -18,18 +20,21 @@ class TestMySolutionG(unittest.TestCase):
         self.assertEqual(bitperebor(5, [6, 7, 8, 9, 10]), None)
 
     def test_vbr_lst(self):
+        """провека функции выбора создования массива"""
         with self.assertRaises(ValueError):
             vbr_lst(-1)
         with self.assertRaises(ValueError):
             vbr_lst(3)
 
     def test_vbr0(self):
+        """провека функции составляющая массив из подряд идущих чисел"""
         self.assertEqual(vbr_0(2, 8), [2, 3, 4, 5, 6, 7, 8])
         self.assertEqual(vbr_0(-2, 5), [-2, -1, 0, 1, 2, 3, 4, 5])
         with self.assertRaises(ValueError):
             vbr_0(5, 2)
 
     def test_vbr1(self):
+        """провека функции составляющая массив из рандомных чисел"""
         with self.assertRaises(ValueError):
             vbr_1(5, 2, 5)
         with self.assertRaises(ValueError):
@@ -40,3 +45,4 @@ class TestMySolutionG(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
