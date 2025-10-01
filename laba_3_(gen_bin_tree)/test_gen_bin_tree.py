b6d3fdf7d3a19c5bb42260a8c6bb0b3c89aa0693
platonpotemkin51-source <platonpotemkin51@gmail.com>
2025-10-01 13:55:26 +0300

    Update test_gen_bin_tree.py

--- laba_3_(gen_bin_tree)/test_gen_bin_tree.py
+++ laba_3_(gen_bin_tree)/test_gen_bin_tree.py
@@ -10,20 +10,22 @@ class TestMySolutionG(unittest.TestCase):
         self.assertEqual(go_left(-1), 0)
         self.assertEqual(go_left(-3), -4)
 
-
     def test_go_right(self):
         """проверка функции go_right на штатную работу"""
         self.assertEqual(go_right(5), 8)
         self.assertEqual(go_right(1), 0)
         self.assertEqual(go_right(-3), -8)
 
-
     def test_gen_bin_tree(self):
         """проверка функции gen_bin_tree (на выдачу ошибки при отрицательном уровне и на штатную работу)"""
         with self.assertRaises(ValueError):
             gen_bin_tree(1, -10)
-        self.assertEqual(gen_bin_tree(2, 2), {'2':[{'6':[None,None]},{'2':[None,None]}]})
-
+        self.assertEqual(gen_bin_tree(2, 2),
+                         {'2': [{
+                             '6': [None, None]
+                         }, {
+                             '2': [None, None]
+                         }]})
 
 
 if __name__ == '__main__':
