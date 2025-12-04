2b963897b3107de710efbaf8df61d239a958b2ae
platonpotemkin51-source <platonpotemkin51@gmail.com>
2025-12-04 20:43:25 +0300

    Delete laba_8/laba_8_on_condition directory

--- laba_8/laba_8_on_condition/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .author import Author
-from .app import App
-from .user import User
-from .currency import Currency
-from .user_currency import UserCurrency
-
-__all__ = ['Author', 'App', 'User', 'Currency', 'UserCurrency']
