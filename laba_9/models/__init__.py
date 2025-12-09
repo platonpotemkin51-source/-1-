ccc6d4aeef770ff683f54e0c3923e63427b071e2
platonpotemkin51-source <platonpotemkin51@gmail.com>
2025-12-09 22:18:47 +0300

    Delete laba_9 directory
    замена файла

--- laba_9/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .author import Author
-from .app import App
-from .user import User
-from .currency import Currency
-from .subscription import UserCurrency
-
-__all__ = ['Author', 'App', 'User', 'Currency', 'UserCurrency']
